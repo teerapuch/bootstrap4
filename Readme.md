acd392eb43396e2f5c445ab3e3399fee910e5640
teerapuch <teerapuch@hotmail.com>
2017-08-12 01:42:52 +0700

    initial first code commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+Bootstrap v4.0 Tutorial
+==============
+
+Start Project With Bootstrap CSS Framework Version 4.0
+<p>
+Bootstrap 4.0 tutorial with Teerapuch at youtube series
+for starter and new web developer who want to use css framework
+for create project website
+</p>
+<p>
+Website : <a href="https://getbootstrap.com">Bootstrap</a>
+==============
+<p>
+Made By <a href="http://twitter.com/teerapuch">@teerapuch</a>
+<br>
+<a href="https://www.youtube.com/watch?v=yFjiMO4H-v0&list=PLSzIGf_nlT5ddBvyOxO3RyE8LAAx9pmLA">Youtube</a>
+<br>
+<a href="https://teerapuch.com">teerapuhc.com</a>
+</p>
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Bootstrap v4.0</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1>Hello Bootstrap v4.0</h1>
+            <div class="row">
+                <div class="col-md-12 border border-dark">
+                    col-md-12
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-6 border border-dark">
+                    col-md-6 col-sm-6 col-xs-6
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6 border border-dark">
+                    col-md-6 col-sm-6 col-xs-6
+                </div>
+            </div>
+
+            <hr>
+
+            <h2>Auto Layout</h2>
+            <div class="row">
+                <div class="col border border-success">
+                    col 12
+                </div>
+            </div>
+            <div class="row">
+                <div class="col border border-success">
+                    col 6
+                </div>
+                <div class="col border border-success">
+                    col 6
+                </div>
+            </div>
+            <div class="row">
+                <div class="col border border-success">
+                    col 3
+                </div>
+                <div class="col border border-success">
+                    col 3
+                </div>
+                <div class="col border border-success">
+                    col 3
+                </div>
+            </div>
+
+            <hr>
+
+            <h2>Auto Fix Column</h2>
+            <div class="row">
+                <div class="col border border-info">
+                    col 2
+                </div>
+                <div class="col-8 border border-info">
+                    col 8
+                </div>
+                <div class="col border border-info">
+                    col 2
+                </div>
+            </div>
+            <div class="row">
+                <div class="col border border-info">
+                    col 3
+                </div>
+                <div class="col-6 border border-info">
+                    col 6
+                </div>
+                <div class="col border border-info">
+                    col 3
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 border border-info">
+                    col 6
+                </div>
+                <div class="col border border-info">
+                    col 3
+                </div>
+                <div class="col border border-info">
+                    col 3
+                </div>
+            </div>
+
+
+        </div><!-- /.container -->
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    </body>
+</html>
